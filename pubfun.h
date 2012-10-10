eaab5c63f499c52a168e8178b15cd3dd05d3e126
zym <zwdnet@163.com>
2012-10-10 19:22:41 +0800

    在Linux里写的记账程序，移植到Windows里，使用了sqlite3数据库，有乱码问题已经解决。

---
+++ pubfun.h
@@ -0,0 +1,45 @@
+#ifndef PUBFUN_H
+#define PUBFUN_H
+/*
+电子记帐软件1.0（命令行版）
+公共函数模块
+作者:zwdnet@163.com
+日期:2011-10-17
+许可协议：GPL3
+最新修改：2012-10-05移植到windows下
+*/
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+//公共函数,照这里写的：http://blog.csdn.net/touzani/article/details/1623850
+//将整数转换为字符串
+template <typename T>
+string num2str(T n)
+{
+	stringstream ss;
+	ss<<n;
+	return ss.str();
+}
+
+//将字符串转换为数字(整数)
+int str2int(string s)
+{
+	int num;
+	stringstream ss(s);
+	ss>>num;
+	return num;
+}
+
+//将字符串转换为数字(小数)
+float str2float(string s)
+{
+	float num;
+	stringstream ss(s);
+	ss>>num;
+	return num;
+}
+
+#endif
